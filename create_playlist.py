557c0aeaf53c2e19839d9c9d4e675e4d075d1e9a
your Shubinder Virk <shubinder92@gmail.com>
2020-03-26 17:13:08 +0000

    Connecting Spotify

--- create_playlist.py
+++ create_playlist.py
@@ -3,21 +3,28 @@
 # Step 3: Create A New Playlist 
 # Step 4: Search For the song 
 # Step 5: Add this song into the new Spotify playlist 
-
+import json 
+from secrets import spotify_user_id
 
 class CreatePlaylist:
 
 # Step 1: Log Into Youtube 
-def get_youtube_client(self):
-    pass
+    def get_youtube_client(self):
+        pass
 
 # Step 2: Grab Our Liked Videos 
-def get_liked_videos(self):
-    pass
+    def get_liked_videos(self):
+        pass
 
 # Step 3: Create A New Playlist 
-def create_playlist(self):
-    pass
+    def create_playlist(self):
+        request_body= json.dumps({
+            "name": "Youtube Liked Vids",
+            "description": "All Liked Youtube Videos",
+            "public": True
+        })
+    
+        query  
 
 # Step 4: Search For the song 
 def get_spotify_uri(self):
