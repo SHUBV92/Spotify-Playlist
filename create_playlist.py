a0747b601968f160c46bf4e330353266f5fdc468
your Shubinder Virk <shubinder92@gmail.com>
2020-03-26 16:29:50 +0000

    first commit

---
+++ create_playlist.py
@@ -0,0 +1,29 @@
+# Step 1: Log Into Youtube 
+# Step 2: Grab Our Liked Videos 
+# Step 3: Create A New Playlist 
+# Step 4: Search For the song 
+# Step 5: Add this song into the new Spotify playlist 
+
+
+class CreatePlaylist:
+
+# Step 1: Log Into Youtube 
+def get_youtube_client(self):
+    pass
+
+# Step 2: Grab Our Liked Videos 
+def get_liked_videos(self):
+    pass
+
+# Step 3: Create A New Playlist 
+def create_playlist(self):
+    pass
+
+# Step 4: Search For the song 
+def get_spotify_uri(self):
+    pass
+
+# Step 5: Add this song into the new Spotify playlist 
+def add_song_to_playlist(self):
+    pass
+
