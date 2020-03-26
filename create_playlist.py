7304b168d2c4b6e5c3818769c90487e2c0df240e
your Shubinder Virk <shubinder92@gmail.com>
2020-03-26 21:55:32 +0000

    Created a create playlist function

--- create_playlist.py
+++ create_playlist.py
@@ -24,13 +24,36 @@ class CreatePlaylist:
             "public": True
         })
     
-        query  
+        query = "https://api.spotify.com/v1/users/{}/playlists".format(self.id)
+        response = request.post(
+            query, 
+            data=request_body,
+            headers={
+                "Content-Type":"application/json",
+                "Authorization":"Bearer {}".format(spotify_token)
+            }
+        )
+        response_json = response.json()
+
+        # playlist id 
+        return response_json("id")
+
 
 # Step 4: Search For the song 
-def get_spotify_uri(self):
-    pass
+def get_spotify_uri(self, track, artist):
+    query   = "https://api.spotify.com/v1/search?query=track%3A{}+"
+    song_name, 
+    artisit
+)
+    response = request.get(
+        query, 
+        header={
+            "Content-Type": "application/json",
+            "Authorization": "Bearer {}".format(spotify_token)
+        }
+    )
 
 # Step 5: Add this song into the new Spotify playlist 
 def add_song_to_playlist(self):
-    pass
+    pass   
 
